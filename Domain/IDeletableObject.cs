9af50138ffa6a1f1e54354f73cbf1567336d72c9
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2021-02-18 14:36:38 +0300

    Добавлены миграции. Исправление глюка с Repository.

---
+++ Domain/IDeletableObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public interface IDeletableObject
+    {
+        bool IsDeleted { get; set; }
+        DateTime DeleteDate { get; set; }
+    }
+}
---
+++ Domain/ProtocolParameter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain
+{
+    public class ProtocolParameter : PersistentObject
+    {
+        public string Name { get; set; }
+        public virtual ICollection<Template> Templates { get; set; }
+        public ProtocolParameter()
+        {
+            Templates = new HashSet<Template>(); 
+        }
+
+    }
+}
