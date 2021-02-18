1edd9f0bf2b92548e64083780040b8c01c86c181
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2021-02-18 15:18:00 +0300

    Добавил repository и Providers в нем

---
+++ Repository/ProtocolParameterProvider/IProtocolParameterProvider.cs
@@ -0,0 +1,10 @@
+﻿using Domain;
+using Repository.Repository;
+
+
+namespace Repository.ProtocolParameterProvider
+{
+    public interface IProtocolParameterProvider : IRepository<ProtocolParameter>
+    {
+    }
+}
---
+++ Repository/ProtocolParameterProvider/ProtocolParameterProvider.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Repository.Repository;
+using Storage;
+
+namespace Repository.ProtocolParameterProvider
+{
+    public class ProtocolParameterProvider : Repository<ProtocolParameter>, IProtocolParameterProvider
+    {
+        public ProtocolParameterProvider(AppDbContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
