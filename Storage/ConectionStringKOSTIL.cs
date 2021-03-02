3a36ca97d590e29f70eb8cb13acb7991334fc8e2
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2021-03-02 13:18:02 +0300

    Версия 1.0 (Работает на папином компе)

--- Storage/ConectionStringKOSTIL.cs
+++ Storage/ConectionStringKOSTIL.cs
@@ -5,7 +5,10 @@ namespace Storage
 {
     public static class ConectionStringKOSTIL
     {
-        public static string NewConectionString()
+        /*
+        public string ConectionString { get; set; }
+
+        ConectionStringKOSTIL()
         {
             string path = @"ConnectionString.txt";
             string conectionString = "";
@@ -21,7 +24,28 @@ namespace Storage
 
                 throw new Exception("НЕТ СТРОКИ ПОДКЛЮЧЕНИЯ К СЕРВЕРУ!");
             }
-            
+
+            this.ConectionString = conectionString;
+        }
+        */
+        public static string NewConectionString()
+        {
+
+            string path = @"ConnectionString.txt";
+            string conectionString = "";
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    conectionString = sr.ReadToEnd();
+                }
+            }
+            catch
+            {
+
+                throw new Exception("НЕТ СТРОКИ ПОДКЛЮЧЕНИЯ К СЕРВЕРУ!");
+            }
+
             return conectionString;
         }
     }
