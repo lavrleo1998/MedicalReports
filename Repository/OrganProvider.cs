f854c9f875e4c82b7f501f0441a40b7cbd58d02c
Леонов Лаврений <67091193+lavrleo1998@users.noreply.github.com>
2021-03-04 17:01:14 +0300

    Версия 1.1

---
+++ Repository/OrganProvider.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+using Storage;
+
+
+namespace Repository
+{
+    public class OrganProvider : Repository<Organ>, IOrganProvider
+    {
+        public OrganProvider(AppDbContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
